cd5be9ab8ce5647ac32b7097bda5f1387145af49
saikigarrett <gsaiki@vcd-ch204-03.eastern.ewu.edu>
2017-05-25 14:36:45 -0700

    Added Banner Image
    We added a banner image to portfolio
    We used Flexbox to make boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 // Scripts
 
+//Store Banner Images
+var img0 = 'https://unsplash.it/600/300'
+var img1 = 'https://unsplash.it/600/300?image=3'
+var img2 = 'https://unsplash.it/600/300?image=7'
+
+// Random Number Generator
+var maxRand = 3;
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
+
+// Swap out Banner Images
+$('header img').attr('src', eval('img' + randNum))
+
+
+
 // Lightgallery
 $('figure').lightGallery({
     selector: 'a',
